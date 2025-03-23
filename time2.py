c9bfc24f978c43d070808e3e0d36981acc851b1c
Anii <109001556+ax8n@users.noreply.github.com>
2025-03-23 07:15:09 +0530

    Add files via upload

---
+++ time2.py
@@ -0,0 +1,16 @@
+import os, requests
+from requests import get
+import sys
+import time
+import webbrowser
+from datetime import datetime
+EXPIRE_TIME = '2025-03-24 08:00:00'
+EXPIRE_MSG = '\033[91m➜ This tool has been stopped by the developer.'
+
+def check_expiration():
+    current_time = datetime.now()
+    expiration_time = datetime.strptime(EXPIRE_TIME, '%Y-%m-%d %H:%M:%S')
+    if current_time > expiration_time:
+        print(EXPIRE_MSG)
+        sys.exit(1)
+check_expiration()
